4d84305c94fce874831f20cb9f111b06339ffb27
FranciscoLi2000 <francisco2000li@icloud.com>
2025-02-03 13:10:51 +0000

    range of putint

--- ex01/ft_range.c
+++ ex01/ft_range.c
@@ -8,7 +8,7 @@ int	*ft_range(int min, int max)
 
 	if (min >= max)
 		return (NULL);
-	size = max - min + 1;
+	size = max - min;
 	values = malloc(size * sizeof(int));
 	if (values == NULL)
 		return (NULL);
